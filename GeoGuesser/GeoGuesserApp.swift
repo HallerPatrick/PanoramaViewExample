700fc3670c03f07827e7739fb755fa18be71f239
Patrick Haller <patrickhaller40@googlemail.com>
2021-06-02 17:09:52 +0200

    Initial Commit

---
+++ GeoGuesser/GeoGuesserApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoGuesserApp.swift
+//  GeoGuesser
+//
+//  Created by Patrick Haller on 02.06.21.
+//
+
+import SwiftUI
+
+@main
+struct GeoGuesserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
